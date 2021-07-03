936b096299361948b293879a06475b65f5e9ebcc
Liam Ngo <70437668+daiphuongngo@users.noreply.github.com>
2021-07-03 18:56:57 -0400

    Delete Weather Observation Station 12.sql

--- Weather Observation Station 12.sql
+++
@@ -1,6 +0,0 @@
-SELECT DISTINCT CITY
-FROM STATION
-WHERE LEFT(CITY, 1) NOT IN ('a','e','i','o','u')
-AND RIGHT(CITY, 1) NOT IN ('a','e','i','o','u');
-
-/* do not start with vowels and do not end with vowels */
