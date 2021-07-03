4326e4389ccecee9c4d17df3282bfafc6719325d
Liam Ngo <70437668+daiphuongngo@users.noreply.github.com>
2021-07-03 18:55:30 -0400

    Delete Employee Salaries.sql

--- Employee Salaries.sql
+++
@@ -1,8 +0,0 @@
-SELECT NAME FROM EMPLOYEE
-WHERE SALARY > 2000 AND MONTHS < 10
-ORDER BY EMPLOYEE_ID;
-
-/* Write a query that prints a list of employee names (i.e.: the name attribute) 
-for employees in Employee having a salary greater than  per month 
-who have been employees for less than 10 months. 
-Sort your result by ascending employee_id. */
