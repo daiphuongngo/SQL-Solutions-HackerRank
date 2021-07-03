29f755cf87b89d4a89542ac1cc2fecf5f16cee6c
Liam Ngo <70437668+daiphuongngo@users.noreply.github.com>
2021-07-03 18:56:53 -0400

    Delete Weather Observation Station 5.sql

--- Weather Observation Station 5.sql
+++
@@ -1,9 +0,0 @@
-SELECT CITY, LENGTH(CITY) FROM STATION
-ORDER BY LENGTH(CITY), CITY ASC
-LIMIT 1;
-SELECT CITY, LENGTH(CITY) FROM STATION
-ORDER BY LENGTH(CITY) DESC
-LIMIT 1;
-
-/* return the city with the longest name and the city with the shortest name (with city name sort order as a tie breaker)
-"limit 1": each query only returns the first result, thus returning the longest and shortest city names.*/
